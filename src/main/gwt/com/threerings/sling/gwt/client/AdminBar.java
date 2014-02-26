945f7c78428a5b8d93ab984044d62e4cffc0955c
Sylvain Royer <sylvain@ooo187.local>
2014-02-25 16:49:06 -0800

    Make the admin bar two lines.

--- src/main/gwt/com/threerings/sling/gwt/client/AdminBar.java
+++ src/main/gwt/com/threerings/sling/gwt/client/AdminBar.java
@@ -6,9 +6,12 @@ package com.threerings.sling.gwt.client;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
 
 import com.threerings.gwt.ui.EnterClickAdapter;
 import com.threerings.gwt.ui.ItemListBox;
@@ -21,7 +24,7 @@ import com.threerings.sling.gwt.util.PageAddress;
 /**
  * The standard toolbar for admin users.
  */
-public class AdminBar extends HorizontalPanel
+public class AdminBar extends VerticalPanel
 {
     /**
      * Creates a new admin toolbar for the given app.
@@ -31,6 +34,7 @@ public class AdminBar extends HorizontalPanel
         _app = app;
         setStyleName("uAdminBar");
         setVerticalAlignment(ALIGN_MIDDLE);
+        setHorizontalAlignment(ALIGN_CENTER);
 
         final ItemListBox<Accounts.SearchBy> accountSearchType =
             ItemListBox.<Accounts.SearchBy>builder()
@@ -45,11 +49,14 @@ public class AdminBar extends HorizontalPanel
 
         Label logout = Widgets.newLabel(_msgs.logout(), "actionLabel");
 
-        add(Widgets.newFlowPanel("Accounts",
+        HorizontalPanel row = Widgets.newRow("AdminRow");
+        add(row);
+
+        row.add(Widgets.newFlowPanel("Accounts",
             Widgets.newLabel(_msgs.searchLabel()),
             accountSearchType, accountQuery));
 
-        add(Widgets.newFlowPanel("Events",
+        row.add(Widgets.newFlowPanel("Events",
             Widgets.newLabel("\u2022", "SpacerLabel"),
             Widgets.newLabel(_msgs.eventsLabel(), "BarLabel"),
             SlingUtils.makeLink(_app.getContext(), _msgs.open(), Events.quickSearchOpen()),
@@ -58,20 +65,22 @@ public class AdminBar extends HorizontalPanel
             eventIdQuery,
             SlingUtils.makeLink(_app.getContext(), _msgs.advanced(), Events.search())));
 
-        add(Widgets.newFlowPanel("LoginStatus",
+        row.add(Widgets.newFlowPanel("LoginStatus",
             Widgets.newLabel("\u2022", "SpacerLabel"),
             Widgets.newLabel(_msgs.loggedInAs(), "BarLabel"),
             Widgets.newLabel(_app.getContext().ainfo.name.accountName),
             logout));
 
         if (_app.getContext().isAdmin()) {
-            add(Widgets.newFlowPanel("CreateAccount",
+            row.add(Widgets.newFlowPanel("CreateAccount",
                 Widgets.newLabel("\u2022", "SpacerLabel"),
                 SlingUtils.makeLink(
                     _app.getContext(), _msgs.createAccount(), Accounts.createAccount())));
         }
 
-        add(Widgets.newFlowPanel("AgentActivity",
+        row = Widgets.newRow("AdminRow");
+        add(row);
+        row.add(Widgets.newFlowPanel("AgentActivity",
             Widgets.newLabel("\u2022", "SpacerLabel"),
             SlingUtils.makeLink(_app.getContext(), _msgs.agentActivity(), Reports.agentActivity())));
 
